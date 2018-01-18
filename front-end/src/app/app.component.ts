60199381f1c8f83c815d2b9576a2c4e9aef047b0
Luke Twomey <luke.twomey85@gmail.com>
2018-01-18 11:53:15 +0000

    Move deposit/withdraw functions from main component into pot model.

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -10,35 +10,6 @@ export class AppComponent {
   pots = [];
 
   addNewPotActive = false;
-  summaryActive = true;
-  depositFundsActive = false;
-  withdrawFundsActive = false;
-
-  depositFunds(){
-    this.depositFundsActive = true;
-    this.summaryActive = false;
-  }
-
-  withdrawFunds(){
-    this.withdrawFundsActive = true;
-    this.summaryActive = false;
-  }
-
-  returnToSummary(){
-    this.depositFundsActive = false;
-    this.withdrawFundsActive = false;
-    this.summaryActive = true;
-  }
-
-  confirmDeposit(depositAmount){
-    //this.potBalance += parseFloat(depositAmount);
-    this.returnToSummary();
-  }
-
-  confirmWithdrawal(withdrawalAmount){
-    //this.potBalance -= parseFloat(withdrawalAmount);
-    this.returnToSummary();
-  }
 
   createPot(potDetails) {
     this.pots.push(new Pot(potDetails.value));
--- front-end/src/app/pot.model.ts
+++ front-end/src/app/pot.model.ts
@@ -6,16 +6,50 @@ export class Pot {
   icon: string;
   progress: string;
 
+  summaryActive: boolean;
+  depositFundsActive: boolean;
+  withdrawFundsActive: boolean;
+
   constructor(potDetails:any) {
     this.name = potDetails.name;
     this.accountName = potDetails.accountName;
     this.balance = 0;
     this.icon = 'assets/holiday.png';
     this.progress = 'assets/progress-bar.jpg';
+
+    this.summaryActive = true;
+    this.depositFundsActive = false;
+    this.withdrawFundsActive = false;
   }
 
   getBalance() {
     return this.balance.toFixed(2);
+  }
+
+  deposit(depositAmount) {
+    this.balance += parseFloat(depositAmount);
+    this.returnToSummary();
+  }
+
+  withdraw(withdrawalAmount) {
+    this.balance -= parseFloat(withdrawalAmount);
+    this.returnToSummary();
+  }
+
+  returnToSummary(){
+    this.depositFundsActive = false;
+    this.withdrawFundsActive = false;
+    this.summaryActive = true;
+  }
+
+  depositFunds(){
+    this.depositFundsActive = true;
+    this.summaryActive = false;
+  }
+
+  withdrawFunds(){
+    this.withdrawFundsActive = true;
+    this.summaryActive = false;
   }
 
 }

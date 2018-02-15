b253fe99812f66c9c92b712ce4668af53ee7b0d3
Luke Twomey <luke.twomey85@gmail.com>
2018-02-15 16:01:51 +0000

    Get more buttons working again, move more methods from model to component to be used for generic pots.

--- back-end/src/shared/pot.model.js
+++ back-end/src/shared/pot.model.js
@@ -36,101 +36,12 @@ class Pot {
     }
   }
 
-  returnToSummary() {
-    this.depositFundsActive = false;
-    this.withdrawFundsActive = false;
-    this.settingsActive = false;
-    this.summaryActive = true;
-    this.preventWithdraw = false;
-  }
-
-  returnToSettings() {
-    this.depositFundsActive = false;
-    this.withdrawFundsActive = false;
-    this.settingsActive = true;
-    this.deleteActive = false;
-  }
-
-  // depositFunds() {
-  //   this.depositFundsActive = true;
-  //   this.summaryActive = false;
-  // }
-
-  // withdrawFunds() {
-  //   this.withdrawFundsActive = true;
-  //   this.summaryActive = false;
-  // }
-
-  changeSettings() {
-    this.settingsActive = true;
-    this.summaryActive = false;
-  }
-
-  updateProgressBar() {
-
-    // Only update the progress bar if a pot target has been set
-    if(this.target > 0) {
-
-      var self = this;
-      var startPoint = this.progress;
-      var endPoint;
-      var progressBarDirection;
-
-      // Endpoint value is where the progress bar needs to end up
-      if(this.balance == 0) {
-        endPoint = 0;
-      } else if(this.balance/this.target <= 1) {
-        endPoint = Math.round((this.balance/this.target) * 100);
-      } else {
-        endPoint = 100;
-      }
-
-      if(endPoint == startPoint) { // No deposit or withdrawal has been made, and the pot target has not been changed
-        this.changeProgressBarColor();
-        return
-      } else if(endPoint > startPoint) { // Deposit made, or pot target reduced
-        progressBarDirection = 'up';
-      } else { // Withdrawal made, or pot target increased
-        progressBarDirection = 'down';
-        this.changeProgressBarColor();
-      }
-
-      // Repeat setInterval until progress bar is in correct position
-      var interval = setInterval(function(){
-
-        if(progressBarDirection == 'up') {
-          startPoint++;
-        } else {
-          startPoint--;
-        }
-
-        self.progress = startPoint;
-
-        if(self.progress == endPoint) {
-          clearInterval(interval);
-          self.changeProgressBarColor();
-        }
-      }, 15);
-
-    }
-  }
-
   changeProgressBarColor() {
     if(this.balance >= this.target) {
       this.progressBarColor = '#f8c40e';
     } else {
       this.progressBarColor = '#06b127';
     }
-  }
-
-  updateSettings() {
-    this.updateProgressBar();
-    this.returnToSummary();
-  }
-
-  deleteConfirm() {
-    this.settingsActive = false;
-    this.deleteActive = true;
   }
 
 }
--- front-end/src/app/pots/pot.component.ts
+++ front-end/src/app/pots/pot.component.ts
@@ -45,15 +45,88 @@ export class PotComponent {
     this.pots[pot.id].preventWithdraw = false;
   }
 
+  changeSettings(pot) {
+    this.pots[pot.id].settingsActive = true;
+    this.pots[pot.id].summaryActive = false;
+  }
+
+  updateSettings(pot) {
+    console.log("Updating settings..." + pot.id);
+    this.updateProgressBar(pot);
+    this.returnToSummary(pot);
+  }
+
+  deleteConfirm(pot) {
+    this.pots[pot.id].settingsActive = false;
+    this.pots[pot.id].deleteActive = true;
+  }
+
+  returnToSettings(pot) {
+    this.pots[pot.id].depositFundsActive = false;
+    this.pots[pot.id].withdrawFundsActive = false;
+    this.pots[pot.id].settingsActive = true;
+    this.pots[pot.id].deleteActive = false;
+  }
+
   // createPot(potDetails) {
   //   this.potService.createPot(potDetails);
   //   this.addNewPotActive = false;
   // }
 
-  // deletePot(potId) {
-  //   this.potService.deletePot(potId);
-  //   this.getPots();
-  // }
+  deletePot(pot) {
+    console.log("Deleting pot..." + pot.id);
+    // this.potService.deletePot(potId);
+    // this.getPots();
+  }
+
+  updateProgressBar(pot) {
+
+    // Only update the progress bar if a pot target has been set
+    if(this.pots[pot.id] > 0) {
+
+      var self = this.pots[pot.id];
+      var startPoint = this.pots[pot.id].progress;
+      var endPoint;
+      var progressBarDirection;
+
+      // Endpoint value is where the progress bar needs to end up
+      if(this.pots[pot.id].balance == 0) {
+        endPoint = 0;
+      } else if(this.pots[pot.id].balance/this.pots[pot.id].target <= 1) {
+        endPoint = Math.round((this.pots[pot.id].balance/this.pots[pot.id].target) * 100);
+      } else {
+        endPoint = 100;
+      }
+
+      if(endPoint == startPoint) { // No deposit or withdrawal has been made, and the pot target has not been changed
+        this.pots[pot.id].changeProgressBarColor();
+        return
+      } else if(endPoint > startPoint) { // Deposit made, or pot target reduced
+        progressBarDirection = 'up';
+      } else { // Withdrawal made, or pot target increased
+        progressBarDirection = 'down';
+        this.pots[pot.id].changeProgressBarColor();
+      }
+
+      // Repeat setInterval until progress bar is in correct position
+      var interval = setInterval(function(){
+
+        if(progressBarDirection == 'up') {
+          startPoint++;
+        } else {
+          startPoint--;
+        }
+
+        self.progress = startPoint;
+
+        if(self.progress == endPoint) {
+          clearInterval(interval);
+          self.changeProgressBarColor();
+        }
+      }, 15);
+
+    }
+  }
 
 }
 
